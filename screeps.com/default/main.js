97cfc493879f449c0be5f06f20a856e2b327a4f9
Anrock <anrock85@gmail.com>
2020-03-11 18:23:12 +0700

    defaultHarvester spawn fix 4

--- screeps.com/default/main.js
+++ screeps.com/default/main.js
@@ -120,7 +120,8 @@ module.exports.loop = function () {
     }
 
     for (let room in Game.rooms) {
-        console.log("Iterating room:" + Game.rooms[room] + ", Energy available: " + Game.rooms[room].energyAvailable + "\\" + Game.rooms[room].energyCapacityAvailable);
+        if (Game.time % 10 === 0)
+            console.log("Iterating room:" + Game.rooms[room] + ", Energy available: " + Game.rooms[room].energyAvailable + "\\" + Game.rooms[room].energyCapacityAvailable);
         populationController.check(Game.rooms[room]);
     }
 };

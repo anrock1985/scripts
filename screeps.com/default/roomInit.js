a40cd36fb20b7089b22a6524b90f16ddbc29524f
Anrock <anrock85@gmail.com>
2020-03-15 00:54:53 +0700

    added resource pool for carry 8

--- screeps.com/default/roomInit.js
+++ screeps.com/default/roomInit.js
@@ -127,7 +127,7 @@ let roomInit = {
             if (room.memory.creeps.length > 0) {
                 for (let i = 0; i < room.memory.creeps.length; i++) {
                     let creep = Game.getObjectById(room.memory.creeps[i]);
-                    // resourcePoolController.check(creep);
+                    resourcePoolController.check(creep);
                     let creepReservedResourceId = creep.memory.reservedResource.id;
                     if (room.memory.resourcePool[creepReservedResourceId]) {
                         room.memory.resourcePool[creepReservedResourceId].amount -= Game.getObjectById(room.memory.creeps[i]).memory.reservedResource.amount;

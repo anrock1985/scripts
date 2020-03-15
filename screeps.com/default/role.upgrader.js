dfe6aeb136baa95d29809741e643fb2c2f693cf2
Anrock <anrock85@gmail.com>
2020-03-15 18:03:23 +0700

    storage pool controller for upgrader role debug 14

--- screeps.com/default/role.upgrader.js
+++ screeps.com/default/role.upgrader.js
@@ -40,7 +40,7 @@ let roleUpgrader = {
             creep.memory.closestStorageId = storagesWithEnoughEnergy.id;
         }
 
-        if (creep.memory.reservedStorageResource) {
+        if (creep.memory.reservedStorageResource.id) {
             if (creep.memory.reservedStorageResource.amount > creep.room.memory.storageResourcePool[creep.memory.reservedStorageResource.id].amount) {
                 console.log("WARN: Reserved storage resource disappears!");
                 creep.memory.reservedStorageResource = {};

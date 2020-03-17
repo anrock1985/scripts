ad2d492603bb10ad097c7ac8f71a91d122eb2c8a
Anrock <anrock85@gmail.com>
2020-03-17 19:16:14 +0700

    upgrader role storage pool implemented

--- screeps.com/default/role.upgrader.js
+++ screeps.com/default/role.upgrader.js
@@ -18,43 +18,53 @@ let roleUpgrader = {
             storagePoolController.releaseWithdraw(creep);
         }
 
-        let closestSpawner = creep.pos.findClosestByPath(FIND_MY_SPAWNS);
-        if (closestSpawner) {
-            creep.memory.closestSpawnerId = closestSpawner.id;
-        }
+        // let closestSpawner = creep.pos.findClosestByPath(FIND_MY_SPAWNS);
+        // if (closestSpawner) {
+        //     creep.memory.closestSpawnerId = closestSpawner.id;
+        // }
+        //
+        // let storages = creep.room.find(FIND_MY_STRUCTURES, {
+        //     filter: (s) => {
+        //         return (s.structureType === STRUCTURE_EXTENSION
+        //             || s.structureType === STRUCTURE_CONTAINER
+        //             || s.structureType === STRUCTURE_STORAGE)
+        //             && s.store[RESOURCE_ENERGY] >= (creep.store.getFreeCapacity(RESOURCE_ENERGY))
+        //     }
+        // });
+        //
+        // let storagesWithEnoughEnergy = {};
+        // if (storages) {
+        //     storagesWithEnoughEnergy = creep.pos.findClosestByPath(storages);
+        // }
+        //
+        // if (!storagesWithEnoughEnergy) {
+        //     creep.memory.closestStorageId = undefined;
+        // } else {
+        //     creep.memory.closestStorageId = storagesWithEnoughEnergy.id;
+        // }
+        //
+        // if (!creep.memory.reservedStorageResource || !creep.memory.reservedStorageResource.id && !creep.memory.upgrading && creep.room.energyAvailable >= 300) {
+        //     let storage;
+        //     let reservedAmount;
+        //     for (let s in creep.room.memory.storageResourcePool) {
+        //         storage = creep.room.memory.storageResourcePool[s];
+        //         reservedAmount = creep.store.getFreeCapacity(RESOURCE_ENERGY);
+        //         if (storage.amount >= reservedAmount) {
+        //             storagePoolController.reserveWithdraw(creep, storage.id,
+        //                 storage.resourceType,
+        //                 reservedAmount);
+        //             break;
+        //         }
+        //     }
+        // }
 
-        let storages = creep.room.find(FIND_MY_STRUCTURES, {
-            filter: (s) => {
-                return (s.structureType === STRUCTURE_EXTENSION
-                    || s.structureType === STRUCTURE_CONTAINER
-                    || s.structureType === STRUCTURE_STORAGE)
-                    && s.store[RESOURCE_ENERGY] >= (creep.store.getFreeCapacity(RESOURCE_ENERGY))
+        if (!creep.memory.reservedStorageResource.id && !creep.memory.upgrading) {
+            let storage = {};
+            if (creep.room.memory.storageResourcePool.id) {
+                storage = findClosestStorageResourceByPath(creep, creep.room.memory.storageResourcePool);
             }
-        });
-
-        let storagesWithEnoughEnergy = {};
-        if (storages) {
-            storagesWithEnoughEnergy = creep.pos.findClosestByPath(storages);
-        }
-
-        if (!storagesWithEnoughEnergy) {
-            creep.memory.closestStorageId = undefined;
-        } else {
-            creep.memory.closestStorageId = storagesWithEnoughEnergy.id;
-        }
-
-        if (!creep.memory.reservedStorageResource || !creep.memory.reservedStorageResource.id && !creep.memory.upgrading && creep.room.energyAvailable >= 300) {
-            let storage;
-            let reservedAmount;
-            for (let s in creep.room.memory.storageResourcePool) {
-                storage = creep.room.memory.storageResourcePool[s];
-                reservedAmount = creep.store.getFreeCapacity(RESOURCE_ENERGY);
-                if (storage.amount >= reservedAmount) {
-                    storagePoolController.reserveWithdraw(creep, storage.id,
-                        storage.resourceType,
-                        reservedAmount);
-                    break;
-                }
+            if (storage.id && !creep.memory.upgrading) {
+                storagePoolController.reserveWithdraw(creep, storage.id, storage.resourceType, creep.store.getFreeCapacity(RESOURCE_ENERGY))
             }
         }
 
@@ -70,6 +80,23 @@ let roleUpgrader = {
             if (creep.upgradeController(Game.getObjectById(creep.room.controller.id)) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(Game.getObjectById(creep.room.controller.id));
             }
+        }
+
+        function findClosestStorageResourceByPath(creep, storagesIds) {
+            let closest;
+            let tmp;
+            let storages = [];
+            for (let s in storagesIds) {
+                storages.push(Game.getObjectById(storagesIds[s].id));
+            }
+
+            tmp = creep.pos.findClosestByPath(storages);
+            if (tmp === null)
+                return undefined;
+            else
+                closest = tmp;
+
+            return {id: closest.id, resourceType: RESOURCE_ENERGY, amount: closest.store[RESOURCE_ENERGY]};
         }
     }
 };

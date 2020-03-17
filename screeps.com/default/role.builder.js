79bcd6d3de4d1008fcaa3629ff8eae2023bca0ae
Anrock <anrock85@gmail.com>
2020-03-17 19:03:32 +0700

    builder role storage pool implemented

--- screeps.com/default/role.builder.js
+++ screeps.com/default/role.builder.js
@@ -3,7 +3,6 @@ let roleBuilder = {
 
         let _ = require('lodash');
 
-        // let constructionController = require('constructionController');
         let storagePoolController = require('storagePoolController');
 
         if (!creep.memory.idle)
@@ -23,41 +22,6 @@ let roleBuilder = {
         if (!creep.memory.closestConstructionSiteId) {
             creep.memory.closestConstructionSiteId = {};
         }
-
-        // let constructionSites;
-        // let closestConstructionSite;
-        //
-        // constructionSites = creep.room.find(FIND_MY_CONSTRUCTION_SITES);
-        //
-        // Memory.myConstructionSiteIds = [];
-        // if (constructionSites.length > 0) {
-        //     for (let c in constructionSites) {
-        //         Memory.myConstructionSiteIds.push(constructionSites[c].id);
-        //     }
-        //     closestConstructionSite = creep.pos.findClosestByRange(constructionSites);
-        //     if (closestConstructionSite) {
-        //         creep.memory.closestConstructionSiteId = closestConstructionSite.id;
-        //     } else {
-        //         creep.memory.closestConstructionSiteId = undefined;
-        //     }
-        // } else {
-        //     creep.memory.newRole = "repairer";
-        // }
-        //
-        // let storages = creep.room.find(FIND_STRUCTURES, {
-        //     filter: (s) => {
-        //         return (s.structureType === STRUCTURE_EXTENSION
-        //             || s.structureType === STRUCTURE_CONTAINER
-        //             || s.structureType === STRUCTURE_SPAWN
-        //             || s.structureType === STRUCTURE_STORAGE)
-        //             && s.store[RESOURCE_ENERGY] >= (creep.store.getFreeCapacity(RESOURCE_ENERGY))
-        //     }
-        // });
-
-        // let storagesWithEnoughEnergy = {};
-        // if (storages) {
-        //     storagesWithEnoughEnergy = creep.pos.findClosestByRange(storages);
-        // }
 
         if (!creep.memory.reservedStorageResource.id && !creep.memory.building) {
             let storage = {};
@@ -79,34 +43,6 @@ let roleBuilder = {
             }
         }
 
-        // if (!storagesWithEnoughEnergy) {
-        //     creep.memory.closestStorageId = undefined;
-        // } else {
-        //     creep.memory.closestStorageId = storagesWithEnoughEnergy.id;
-        // }
-
-        // if (creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0 && !creep.memory.building) {
-        //     if (creep.memory.closestStorageId) {
-        //         creep.memory.idle = undefined;
-        //         if (storagesWithEnoughEnergy.structureType === STRUCTURE_CONTAINER) {
-        //             if (creep.withdraw(Game.getObjectById(creep.memory.closestStorageId), RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-        //                 creep.moveTo(Game.getObjectById(creep.memory.closestStorageId));
-        //             }
-        //         } else if (creep.room.energyAvailable >= 300 && Memory.harvesters > 1) {
-        //             if (creep.withdraw(Game.getObjectById(creep.memory.closestStorageId), RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-        //                 creep.moveTo(Game.getObjectById(creep.memory.closestStorageId));
-        //             }
-        //         }
-        //     }
-        // }
-        //
-        // if (creep.memory.closestConstructionSiteId && creep.store[RESOURCE_ENERGY] !== 0 && creep.memory.building) {
-        //     creep.memory.idle = undefined;
-        //     if (creep.memory.closestConstructionSiteId && creep.build(Game.getObjectById(creep.memory.closestConstructionSiteId)) === ERR_NOT_IN_RANGE) {
-        //         creep.moveTo(Game.getObjectById(creep.memory.closestConstructionSiteId));
-        //     }
-        // }
-
         if (creep.memory.reservedStorageResource.id && creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0 && !creep.memory.building) {
             creep.memory.idle = undefined;
             if (creep.withdraw(Game.getObjectById(creep.memory.reservedStorageResource.id), RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
@@ -120,8 +56,6 @@ let roleBuilder = {
                 creep.moveTo(Game.getObjectById(creep.memory.closestConstructionSiteId.id));
             }
         }
-
-        // constructionController.buildExtensions(creep);
 
         function findClosestStorageResourceByPath(creep, storagesIds) {
             let closest;

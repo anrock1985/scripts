7a5da49ebda17d104b72d9b4f9136868632634cc
Anrock <anrock85@gmail.com>
2020-03-17 21:24:09 +0700

    repairer role storage pool implemented

--- screeps.com/default/role.repairer.js
+++ screeps.com/default/role.repairer.js
@@ -43,58 +43,7 @@ let roleRepairer = {
                 creep.memory.closestDamagedStructureId.id = damagedStructure.id;
             }
         }
-
-        //room.memory.myDamagedStructuresIds
-
-
-        // let damagedStructures = creep.room.find(FIND_STRUCTURES, {
-        //     filter: (s) => {
-        //         return ((s.structureType !== STRUCTURE_CONTROLLER) && (s.hits <= (s.hitsMax - (s.hitsMax / 6))));
-        //     }
-        // });
-        //
-        // if (damagedStructures === null) {
-        //     creep.memory.closestDamagedStructureId = undefined;
-        // } else {
-        //     let closestDamagedStructure = creep.pos.findClosestByRange(damagedStructures);
-        //     if (closestDamagedStructure === null) {
-        //         creep.memory.closestDamagedStructureId = undefined;
-        //     } else {
-        //         creep.memory.closestDamagedStructureId = closestDamagedStructure.id;
-        //     }
-        // }
-        //
-        // let storages = creep.room.find(FIND_STRUCTURES, {
-        //     filter: (s) => {
-        //         return (s.structureType === STRUCTURE_EXTENSION
-        //             || s.structureType === STRUCTURE_CONTAINER
-        //             || s.structureType === STRUCTURE_SPAWN
-        //             || s.structureType === STRUCTURE_STORAGE)
-        //             && s.store[RESOURCE_ENERGY] >= (creep.store.getFreeCapacity(RESOURCE_ENERGY))
-        //     }
-        // });
-        //
-        // let storagesWithEnoughEnergy = {};
-        // if (storages) {
-        //     storagesWithEnoughEnergy = creep.pos.findClosestByRange(storages);
-        // }
-        //
-        // if (!storagesWithEnoughEnergy) {
-        //     creep.memory.closestStorageId = undefined;
-        // } else {
-        //     creep.memory.closestStorageId = storagesWithEnoughEnergy.id;
-        // }
-        //
-        // if (creep.memory.repairing === undefined) {
-        //     creep.memory.repairing = true;
-        // }
-        // if (creep.store[RESOURCE_ENERGY] === 0 && creep.memory.repairing) {
-        //     creep.memory.repairing = false;
-        // }
-        // if (creep.store.getFreeCapacity(RESOURCE_ENERGY) === 0) {
-        //     creep.memory.repairing = true;
-        // }
-
+        
         if (creep.memory.closestDamagedStructureId.id && creep.store[RESOURCE_ENERGY] !== 0 && creep.memory.repairing) {
             creep.memory.idle = undefined;
             if (creep.repair(Game.getObjectById(creep.memory.closestDamagedStructureId.id)) === ERR_NOT_IN_RANGE) {

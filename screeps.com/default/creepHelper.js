0add9ce5ef0313f783856c9d872df6d807bbaa1d
Anrock <anrock85@gmail.com>
2020-03-19 19:56:01 +0700

    repairer debug

--- screeps.com/default/creepHelper.js
+++ screeps.com/default/creepHelper.js
@@ -57,12 +57,15 @@ function findClosestIdByPath(creep, ids) {
         idsObjects.push(Game.getObjectById(ids[s]));
     }
 
+    Memory.debugIdsObjects = idsObjects;
+    
     tmp = creep.pos.findClosestByPath(idsObjects);
     if (tmp === null)
         return undefined;
     else
         closest = tmp;
 
+    console.log(closest.id);
     return {id: closest.id};
 }
 

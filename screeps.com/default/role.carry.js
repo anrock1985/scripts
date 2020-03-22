fbd7397b69e4a07a34f5658874d60a4dd2a8b7d4
Anrock <anrock85@gmail.com>
2020-03-22 10:27:32 +0700

    carry tombstone fix 3

--- screeps.com/default/role.carry.js
+++ screeps.com/default/role.carry.js
@@ -68,7 +68,7 @@ let roleCarry = {
 
         if (creep.memory.reservedResource && creep.memory.reservedResource.id) {
             creep.memory.idle = undefined;
-            if (Game.getObjectById(creep.memory.reservedResource.id).creep) {
+            if (Game.getObjectById(creep.memory.reservedResource.id).deathTime) {
                 creep.memory.closestTombstone = {};
                 creep.memory.closestTombstone.id = creep.memory.reservedResource.id;
             }

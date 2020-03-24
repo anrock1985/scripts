d761343dfe3d6dbbb759044d90b1b69db89fb8a7
Anrock <anrock85@gmail.com>
2020-03-24 14:36:42 +0700

    source pool debug 2

--- screeps.com/default/roomHelper.js
+++ screeps.com/default/roomHelper.js
@@ -1,9 +1,9 @@
 function hasIdleCreeps(room, role) {
     if (room.memory.idlers && room.memory.idlers[role]) {
-        console.log("WARN: " + room.name + " has idle " + role.toUpperCase());
+        // console.log("WARN: " + room.name + " has idle " + role.toUpperCase());
         return true;
     } else {
-        console.log("WARN: " + room.name + " has NO idle " + role.toUpperCase());
+        // console.log("WARN: " + room.name + " has NO idle " + role.toUpperCase());
         return false;
     }
 }
